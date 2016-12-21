a118bf288491ba3aba1bc9d5b6b34d510b3acab3
Anton Afanasyev <fun@bk.ru>
2016-12-21 13:39:13 +0000

    Add upgrade script for Ubuntu

---
+++ upgrade-system.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -eux
+
+# apt packages 
+sudo apt upgrade
+sudo apt update
+sudo apt install g++ tmux emacs git python python-pip python-virtualenv
+
+# Go install
+wget 'https://storage.googleapis.com/golang/go1.7.4.linux-amd64.tar.gz'
+sudo tar -C /usr/local -xzf go1.7.4.linux-amd64.tar.gz
+
+# Nodejs
+curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Docker install
+# https://docs.docker.com/engine/installation/linux/ubuntulinux/
